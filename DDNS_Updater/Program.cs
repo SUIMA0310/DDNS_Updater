64ec345cf23b1da0d081116815ae18c9007d6471
ASUS-MAINPC\SUIMA <mizumacrocus@yahoo.co.jp>
2017-11-12 05:54:40 +0000

    Projectの追加

---
+++ DDNS_Updater/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DDNS_Updater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
